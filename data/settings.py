c28495d284b164d22a276a376e278d925ef710b6
Raylen Margono <raylenmargono@Raylens-MBP-2.fios-router.home>
2020-12-17 21:06:36 -0500

    asdfg

---
+++ data/settings.py
@@ -0,0 +1,8 @@
+DB_SETTINGS = {
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'password': 'postgres',
+    'port': 5432,
+    'host': '127.0.0.1'
+}
+OURA_PERSONAL_TOKEN = '<PUT YOUR OWN HERE>'
